a8ce72bf49ff85b7f8e718dc56008f5106510372
N Syrotiuk <ns@localhost>
2017-12-21 21:58:47 +0000

    updated App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,115 @@
+/* Pieces of data required
+
+- game status: not started, started, game over, match over
+- score
+- current player
+- computer move
+- player move
+- game winner
+- match winner
+- [rock, paper, scissors]
+
+Our state is:
+- game started
+- game over
+- match over
+- score, e.g. [2, 3]
+- current player
+- computer move
+- player move
+- game winner (calculated)
+- match winner (calculated)
+
+*/
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button, Grid, Row, Col, Image } from 'react-bootstrap';
+import { Button, Grid, Col, Image } from 'react-bootstrap';
 import './App.css';
 
-
-
-class App extends Component {
-startGame() {
-  console.log('start button clicked');
+class Header extends Component {
+  render() {
+    return (
+      <Col className='header'>Roshambo <small>Rock, paper, scissors</small></Col>
+    );
+  }
 }
-render() {
-  return (
-    <Grid className='grid'>
-      <Row className='header'>
-         <Col>Roshambo <small>Rock, paper, scissors</small></Col>
-      </Row>
-      <Row className='computer'>
+
+class Computer extends Component {
+  render() {
+    return (
+      <div className='computer'>
          <Col>Computer's move</Col>
          <Button bsSize='small'>?</Button>
-      </Row>
-      <Row className='player'>
+      </div>
+    );
+  }
+}
+
+class Player extends Component {
+  render() {
+    return (
+      <div className='player'>
          <Col>Player's move</Col>
          <Button bsSize='small'>Rock</Button>
          <Button bsSize='small'>Paper</Button>
          <Button bsSize='small'>Scissors</Button>
-      </Row>
-      <Row className='status'>
-         <Col className='score'>Computer:&nbsp;&nbsp; 0</Col>
-         <Col className='score'>Player:&nbsp;&nbsp; 0</Col>
-         <Button className='startbutton' bsSize='small' onClick={this.startGame}>Start game</Button>
-      </Row>
-      <Row>
+      </div>
+    );
+  }
+}
+
+class Status extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameStarted: false
+    };
+    this.startGame = this.startGame.bind(this);
+  }
+
+  startGame() {
+    console.log('start button clicked');
+    this.setState(prevState => ({
+      gameStarted: !prevState.gameStarted
+    }));
+  }
+
+  render() {
+    return (
+      <div className='status'>
+        <Col className='bestof'>Best of 5 match</Col>
+        <Col>Computer: 0</Col>
+        <Col>Player: 0</Col>
+        <Button className='startbutton' bsSize='small' onClick={this.startGame}>{this.state.gameStarted ? 'Quit' : 'Start game'}</Button>
+      </div>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
         <Col className='footer'>Powered by React</Col>
         <Image className='App-logo' alt='logo' src={logo}/>
-      </Row>
-    </Grid>
-   );
+      </div>
+    );
+  }
 }
+
+class App extends Component {
+  render() {
+    return (
+      <Grid className='grid'>
+        <Header />
+        <Computer />
+        <Player />
+        <Status />
+        <Footer />
+      </Grid>
+    );
+  }
 }
 
 export default App;
